8da8a0a9bd7d99050ebc922962ff85b213ed46e6
Matteo Turri <matteo.turri@buildo.io>
2018-01-09 17:13:40 +0100

    New currencies added

--- api/src/main/scala/cryptonite/model/Currency.scala
+++ api/src/main/scala/cryptonite/model/Currency.scala
@@ -8,6 +8,23 @@ import io.buildo.enumero.annotations.indexedEnum
   object BitcoinCash { CurrencyInfo ("BCH", true) }
   object Litecoin { CurrencyInfo ("LTC", true) }
   object Ethereum { CurrencyInfo ("ETH", true) }
+  object Ripple { CurrencyInfo ("XRP", true) }
+  object Cardano { CurrencyInfo("ADA", true) }
+  object NEM { CurrencyInfo("XEM", true) }
+  object Stellar { CurrencyInfo("XLM", true) }
+  object IOTA { CurrencyInfo("IOTA", true) }
+  object TRON { CurrencyInfo("TRX", true) }
+  object Dash { CurrencyInfo("DASH", true) }
+  object NEO { CurrencyInfo("NEO", true) }
+  object Monero { CurrencyInfo("XMR", true) }
+  object EOS { CurrencyInfo("EOS", true) }
+  object ICON { CurrencyInfo("ICX", true) }
+  object Qtum { CurrencyInfo("QTUM", true) }
+  object BitcoinGold { CurrencyInfo("BTG", true) }
+  object Lisk { CurrencyInfo("LSK", true) }
+  object RaiBlocks { CurrencyInfo ("XRB", true) }
+  object EthereumClassic { CurrencyInfo ("ETC", true) }
   object Euro { CurrencyInfo ("EUR", false) }
   object Dollar { CurrencyInfo ("USD", false) }
+  object Pound { CurrencyInfo ("GBP", false) }
 }
