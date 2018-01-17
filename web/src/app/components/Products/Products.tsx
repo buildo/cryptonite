1f438ad84f7f40aed7690bffdf307a4312fc596c
Matteo Turri <matteo.turri@buildo.io>
2018-01-17 17:36:30 +0100

    Currency map added, currency removed in bid and ask columns

--- web/src/app/components/Products/Products.tsx
+++ web/src/app/components/Products/Products.tsx
@@ -1,6 +1,32 @@
 import * as React from 'react';
-import { Book, Product, Amount } from 'model';
+import { Book, Product, Amount, Currency } from 'model';
 import { Tablo, Column, Cell, Header } from 'Basic/Tablo';
+
+const currencyMap: { [k in Currency]: string } = {
+  Bitcoin: 'BTC',
+  BitcoinCash: 'BCH',
+  Litecoin: 'LTC',
+  Ethereum: 'ETH',
+  Ripple: 'XRP',
+  Cardano: 'ADA',
+  NEM: 'XEM',
+  Stellar: 'XLM',
+  IOTA: 'IOTA',
+  TRON: 'TRX',
+  Dash: 'DASH',
+  NEO: 'NEO',
+  Monero: 'XMR',
+  EOS: 'EOS',
+  ICON: 'ICX',
+  Qtum: 'QTUM',
+  BitcoinGold: 'BTG',
+  Lisk: 'LSK',
+  RaiBlocks: 'XRB',
+  EthereumClassic: 'ETC',
+  Euro: 'EUR',
+  Dollar: 'USD',
+  Pound: 'GBP'
+}
 
 type Props = {
   books: Array<Book>
@@ -19,22 +45,22 @@ export default class Products extends React.PureComponent<Props> {
 
         <Column name='product' width={200}>
           <Header>Products</Header>
-          <Cell>{ (product: Product) => `${product.base}-${product.quote}` }</Cell>
+          <Cell>{ (product: Product) => `${currencyMap[product.base]}-${currencyMap[product.quote]}` }</Cell>
         </Column>
 
         <Column name='bid' width={200}>
           <Header>Bid</Header>
-          <Cell>{ (amount: Amount) => `${amount.value} ${amount.currency}`}</Cell>
+          <Cell>{ (amount: Amount) => `${amount.value}`}</Cell>
         </Column>
 
         <Column name='ask' width={200}>
           <Header>Ask</Header>
-          <Cell>{ (amount: Amount) => `${amount.value} ${amount.currency}`}</Cell>
+          <Cell>{ (amount: Amount) => `${amount.value}`}</Cell>
         </Column>
 
         <Column name='value' width={200}>
           <Header>Value</Header>
-          <Cell>{ (amount: Amount) => `${amount.value} ${amount.currency}`}</Cell>
+          <Cell>{ (amount: Amount) => `${amount.value} ${currencyMap[amount.currency]}`}</Cell>
         </Column>
       </Tablo>
     )
