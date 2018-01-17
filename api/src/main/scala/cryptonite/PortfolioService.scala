130ecd4ce5fae24e1990f872635badf9941930c9
Matteo Turri <matteo.turri@buildo.io>
2018-01-17 15:24:01 +0100

    Initial implementation

--- api/src/main/scala/cryptonite/PortfolioService.scala
+++ api/src/main/scala/cryptonite/PortfolioService.scala
@@ -2,7 +2,9 @@ package cryptonite
 
 import scala.concurrent.{Future, ExecutionContext}
 
-import cryptonite.model.Amount
+import io.buildo.enumero.CaseEnumSerialization
+
+import cryptonite.model.{ Amount, Currency }
 import cryptonite.errors.ApiError
 
 class PortfolioService(repository: PortfolioRepository)(implicit ec: ExecutionContext) {
@@ -15,6 +17,15 @@ class PortfolioService(repository: PortfolioRepository)(implicit ec: ExecutionCo
     repository.get(code) match {
       case Some(amount) => Right(amount)
       case None => Left(ApiError.CurrencyNotFoundError)
+    }
+  }
+
+  def save(amounts: List[Amount]): Future[Either[ApiError, Unit]] = Future {
+    val currenciesSet = CaseEnumSerialization[Currency].values
+    val currenciesSize = currenciesSet.size
+    (amounts.size, amounts.map(_.currency).toSet) match {
+      case (currenciesSize, currenciesSet) => Right(repository.save(amounts))
+      case _ => Left(ApiError.PortfolioMismatchError)
     }
   }
 }
